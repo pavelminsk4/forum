aef93b80ad237d027fc4083cb279fd428d27d78b
pavelminsk4 <pavelminsk4@gmail.com>
2021-09-01 10:09:35 +0300

    feat: add Logout button

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
       get 'results'
     end
   end
-
+  
   root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
